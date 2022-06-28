bed21d524ea1732838d71f2912fe5dcf53f39414
Aubuty <26222204778@qq.com>
2022-06-28 22:21:34 +0800

    Site updated: 2022-06-28 22:21:34

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","657b4691c1029ac2b2f7f10050b0bdb4"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
